85e2597369fb433cf91b94c576853800a86ff71d
Yoavz0507 <yoav.ziberski@gmail.com>
2025-01-08 15:50:02 +0200

    network

--- User.java
+++ User.java
@@ -54,7 +54,7 @@
     /** Makes this user follow the given name. If successful, returns true. 
      *  If this user already follows the given name, or if the follows list is full, does nothing and returns false; */
     public boolean addFollowee(String name) {
-        if(fCount>10){
+        if(fCount<10){
             return false;
         }
             if(this.follows(name)==false){
